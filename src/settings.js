a2d83299387359f052c6636981e8576a7871c38b
coyeking <771405624@qq.com>
2023-11-27 06:26:28 +0800

    1

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","3d","skybox"],collisionMatrix:[[true],[false,false],[false,false,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/main/main.fire",orientation:"portrait",jsList:["assets/libs/base_lib.js"]};
